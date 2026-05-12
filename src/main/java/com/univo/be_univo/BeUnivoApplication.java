504f3df78c0c6935180b741a08b7a1561d5619b6
PhuongHo105 <22521160@gm.uit.edu.vn>
2026-05-12 22:15:37 +0700

    init project

---
+++ src/main/java/com/univo/be_univo/BeUnivoApplication.java
@@ -0,0 +1,13 @@
+package com.univo.be_univo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeUnivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeUnivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univo/be_univo/BeUnivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univo.be_univo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeUnivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
